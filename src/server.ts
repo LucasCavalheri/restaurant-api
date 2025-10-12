2485d91275094bc5be914331d9ea62b1d6b93e79
LucasCavalheri <lucas.dev.carvalho@gmail.com>
2025-10-12 18:49:17 -0300

    estrutura inicial

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import { routes } from './routes'
 
 const PORT = 3333
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/api', routes)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
