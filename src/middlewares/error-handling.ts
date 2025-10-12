0ab1d509edaa865012b029ba194b275f3a940034
LucasCavalheri <lucas.dev.carvalho@gmail.com>
2025-10-12 20:36:55 -0300

    configuracao do ZodError e instancia do Knex

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,9 +1,14 @@
-import { AppError } from '@/utils/app-error'
 import { Request, Response, NextFunction } from 'express'
+import { ZodError } from 'zod'
+import { AppError } from '@/utils/app-error'
 
 export function errorHandling(error: Error, _request: Request, response: Response, _next: NextFunction) {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({ message: error.message })
+  }
+
+  if (error instanceof ZodError) {
+    return response.status(400).json({ message: 'Erro de validação', issues: error.format() })
   }
 
   return response.status(500).json({ message: 'Houve um erro interno no servidor', error: error.message })
